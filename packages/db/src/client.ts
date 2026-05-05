dce9d87bb0ed5551ddd5f133aa66b5cac0178e1e
Rowan McIntosh <rowanmcintosh7@gmail.com>
2026-05-06 07:42:38 +1000

    read DATABASE_URL directly from process.env

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,28 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { env } from "@repo/env/web";
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
 export const createClient = () => {
   if (global.prisma) {
     return global.prisma;
   }
-
-  const URL = env.DATABASE_URL;
-
+  const URL = process.env.TEST_DATABASE_URL || process.env.DATABASE_URL;
+  if (!URL) {
+    throw new Error(
+      "DATABASE_URL (or TEST_DATABASE_URL) is not set. Set it to something like: file:./dev.db",
+    );
+  }
   const prisma = new PrismaClient({
     datasourceUrl: URL,
   });
-
   console.log("Connected to database");
   console.log(URL);
-
   global.prisma = prisma;
   return prisma;
 };
-
 export const client = {
   get db() {
     return createClient();
